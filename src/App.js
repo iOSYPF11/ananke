5acaaa1303c634b270be486ca28d92d30d1afc04
“杨朋飞” <yours910809@163.com>
2021-08-25 14:18:18 +0800

    add user auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//在App.js中添加身份验证流
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Ananke</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
+
